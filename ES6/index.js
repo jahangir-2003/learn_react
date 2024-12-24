066eab8ed13b6130e48bcae4b8557737f625aa35
jahangir <jahangiralamnke786@gmail.com>
2024-12-24 17:21:39 +0530

    reinitializationn

--- ES6/index.js
+++ ES6/index.js
@@ -1,1 +1,16 @@
-console.log("bjhb");
+let variable = 42;
+const a = 5;
+
+console.log("let ==>", variable);
+console.log("const =>", a);
+
+variable = 50; // it means when we initial any variable using let then we can reinitialize
+// a = 55;//we will get error this means we can't reinitialize const variable
+console.log(variable);
+console.log("const ->", a);
+
+// let ==>let is for variables that you may want to reassign and let is block scope
+
+// const =>const is for variables that you don’t want to reassign and const is also block scope
+
+// Example====>
