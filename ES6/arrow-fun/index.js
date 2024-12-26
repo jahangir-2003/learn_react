7e27ee2d71ffc81088169e3fec4edc7ab18980ac
jahangir <jahangiralamnke786@gmail.com>
2024-12-26 12:45:55 +0530

    learn Arrow function

---
+++ ES6/arrow-fun/index.js
@@ -0,0 +1,14 @@
+const sum = (a, b) => {
+  return a + b;
+};
+console.log(sum(1, 5));
+
+let multi = (a, b) => {
+  return a * b;
+};
+
+console.log(multi(12, 65));
+
+const hello = (a) => "Hello World! " + a;
+
+console.log(hello(12));
